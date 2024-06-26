925c5d8c3cccd038fedf1c839b0112f69f5c750c
DIGI Byte <digimbyte@gmail.com>
2024-06-27 00:44:18 +1000

    small fixes

--- assets/utils.js
+++ assets/utils.js
@@ -1,8 +1,8 @@
 // Function to format file names
 function sanitizeName(input) {
-    // const baseName = input.replace(/\.(md)$/i, '');
-    const baseName = input.replace('.md','')//input.replace(/\.(html|md)$/i, '');// |js|yaml|yml|json
-    const withSpaces = baseName.replace('_',' ')//(/[_-]/g, ' ');
+    const baseName = input.replace(/\.(md)$/i, '');
+    // const baseName = input.replace(/\.(html|md|js|yaml|yml|json)$/i, '');// 
+    const withSpaces = baseName.replace(/[_-]/g, ' ');
 
     // List of filler words to exclude from capitalization
     const fillerWords = ['in', 'of', 'the', 'and', 'a', 'to', 'for', 'with'];
@@ -18,27 +18,7 @@ function sanitizeName(input) {
 }
 
 function getQueryParams(item) {
-    // Create a URLSearchParams object from the query string
     const params = new URLSearchParams(window.location.search);
-    // Get the value of the 'path' query parameter
     const path = params.get(item);
     return path;
 }
-
-// // Function to load and parse a JSON file
-// async function loadJson(url) {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//     return response.json();
-// }
-
-// // Function to load and parse a Markdown file
-// async function loadURL(url) {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//     return response.text();
-// }
